54aad5baa272baeff6d7778a8f18b68acbe4cef4
rafciesielski <rafciesielski@gmail.com>
2016-12-28 21:19:47 +0100

    winningPercentage

--- src/main/kotlin/org/nfllook/tools/dataexport/DataExporter.kt
+++ src/main/kotlin/org/nfllook/tools/dataexport/DataExporter.kt
@@ -4,6 +4,9 @@ import org.nfllook.tools.generated.gd.Away
 import org.nfllook.tools.generated.gd.Home
 import org.nfllook.tools.generated.wst.Team
 import java.io.File
+import java.math.BigDecimal
+import java.math.MathContext
+import java.math.RoundingMode
 import java.util.*
 
 
@@ -54,6 +57,19 @@ class DataExporter(val path: String, val season: Int, val dataService: DataServi
             homeTeam.draws++
             awayTeam.draws++
         }
+        homeTeam.winningPercentage = calcWinningPercentage(homeTeam.wins!!, homeTeam.losses!!)
+        awayTeam.winningPercentage = calcWinningPercentage(awayTeam.wins!!, awayTeam.losses!!)
+    }
+
+    private fun calcWinningPercentage(w: Int, l: Int): Int {
+        if(l == 0) {
+            return 100
+        } else {
+            val wins = BigDecimal.valueOf(w.toDouble())
+            val losses = BigDecimal.valueOf(l.toDouble())
+            val res = wins.divide((wins.plus(losses)), 3, RoundingMode.HALF_DOWN).multiply(BigDecimal(100))
+            return res.round(MathContext(0, RoundingMode.HALF_DOWN)).toInt()
+        }
     }
 
     private fun calcOppsTeamsRecords(week: Int) {
@@ -79,6 +95,8 @@ class DataExporter(val path: String, val season: Int, val dataService: DataServi
                     team.oppsOfRemainingGames.losses = team.oppsOfRemainingGames.losses + opponent.losses
                 }
             }
+            team.oppsOfPlayedGames.winningPercentage = calcWinningPercentage(team.oppsOfPlayedGames.wins!!, team.oppsOfPlayedGames.losses!!)
+            team.oppsOfRemainingGames.winningPercentage = calcWinningPercentage(team.oppsOfRemainingGames.wins!!, team.oppsOfRemainingGames.losses!!)
         }
     }
 
